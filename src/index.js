18679649ec89bfc21cbba475fed0c12f7dee8362
Natanael Arndt <arndtn@gmail.com>
2019-02-14 13:56:03 +0100

    Improve eyecandyness

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import 'bootstrap';
 import './main.scss';
+import fullpage from 'fullpage.js';
+
+var fullPageInstance = new fullpage('#fullpage', {
+  navigation: false,
+  sectionsColor:['#a7d1e2ff', '#b72c37ff', '#215063ff', '#81bdd5ff', '#3887a7ff', '#b64d55ff'],
+  anchors:['Top', 'Programm', 'Location', 'Team', 'PreviousEvents']
+});
